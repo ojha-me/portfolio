1b75d782664599955d2baf0c87645f60a52830fc
Dinesh Ojha <dineshojha1013@gmail.com>
2025-10-26 15:23:22 +0545

    please work.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    // Disable Lightning CSS to avoid native binary issues on Node 24
+    optimizeCss: false,
+  },
 };
 
 export default nextConfig;
